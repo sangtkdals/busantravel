17ef39cc6408556d30d3d9ee3b2c55b733063dfe
dita_810 <dita_810@DESKTOP-US97KA7>
2025-08-06 12:45:56 +0900

    수정

--- src/busantravel/main/Main.java
+++ src/busantravel/main/Main.java
@@ -1,14 +1,9 @@
 package busantravel.main;
 
-import javax.swing.JFrame;
-
-import busantravel.ui.HomePanel;
-
 public class Main {
 
 	public static void main(String[] args) {
-		JFrame j = new JFrame();
-		new HomePanel(j);
+		
 	}
 
 }
